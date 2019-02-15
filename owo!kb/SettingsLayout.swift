098ccf8da2e535cc69f7f4cb10f33203b5fa6941
Ilya Dobryakov <ilya.dobryakov5@gmail.com>
2019-02-15 23:20:06 +0300

    implement button keys change

--- owo!kb/SettingsLayout.swift
+++ owo!kb/SettingsLayout.swift
@@ -8,20 +8,8 @@
 
 import Foundation
 import UIKit
-import Socket
 
 class SettingsLayout: UITableViewController, UITextFieldDelegate {
-    
-    
-    @IBOutlet weak var LayoutButtonsNum: UITextField!
-    @IBAction func LayoutButtonsNumAction(_ sender: Any) {
-        if Int(LayoutButtonsNum.text!)! > 9 {
-            LayoutButtonsNum.text = "9"
-        } else if Int(LayoutButtonsNum.text!)! < 1 {
-            LayoutButtonsNum.text = "1"
-        }
-        UserDefaults.standard.set(LayoutButtonsNum.text, forKey: "ButtonsCount")
-    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if self.restorationIdentifier == "layoutListingScreen" {
@@ -48,10 +36,76 @@ class SettingsLayout: UITableViewController, UITextFieldDelegate {
             }
         }
     }
+
+    
+    @IBOutlet weak var LayoutButtonsNum: UITextField!
+    @IBAction func LayoutButtonsNumAction(_ sender: Any) {
+        if Int(LayoutButtonsNum.text!)! > 9 {
+            LayoutButtonsNum.text = "9"
+        } else if Int(LayoutButtonsNum.text!)! < 1 {
+            LayoutButtonsNum.text = "1"
+        }
+        UserDefaults.standard.set(LayoutButtonsNum.text, forKey: "ButtonsCount")
+    }
+    
+    @IBOutlet weak var ButtonKey1: UITextField!
+    @IBAction func ButtonKey1Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey1.text!.first!).uppercased(), forKey: "ButtonKey1")
+    }
+    
+    @IBOutlet weak var ButtonKey2: UITextField!
+    @IBAction func ButtonKey2Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey2.text!.first!).uppercased(), forKey: "ButtonKey2")
+    }
+    
+    @IBOutlet weak var ButtonKey3: UITextField!
+    @IBAction func ButtonKey3Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey3.text!.first!).uppercased(), forKey: "ButtonKey3")
+    }
+    
+    @IBOutlet weak var ButtonKey4: UITextField!
+    @IBAction func ButtonKey4Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey4.text!.first!).uppercased(), forKey: "ButtonKey4")
+    }
+    
+    @IBOutlet weak var ButtonKey5: UITextField!
+    @IBAction func ButtonKey5Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey5.text!.first!).uppercased(), forKey: "ButtonKey5")
+    }
+    
+    @IBOutlet weak var ButtonKey6: UITextField!
+    @IBAction func ButtonKey6Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey6.text!.first!).uppercased(), forKey: "ButtonKey6")
+    }
+    
+    @IBOutlet weak var ButtonKey7: UITextField!
+    @IBAction func ButtonKey7Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey7.text!.first!).uppercased(), forKey: "ButtonKey7")
+    }
+    
+    @IBOutlet weak var ButtonKey8: UITextField!
+    @IBAction func ButtonKey8Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey8.text!.first!).uppercased(), forKey: "ButtonKey8")
+    }
+    
+    @IBOutlet weak var ButtonKey9: UITextField!
+    @IBAction func ButtonKey9Action(_ sender: Any) {
+        UserDefaults.standard.set(String(ButtonKey9.text!.first!).uppercased(), forKey: "ButtonKey9")
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         if self.restorationIdentifier == "layoutSettingsScreen" {
             LayoutButtonsNum.text = UserDefaults.standard.string(forKey: "ButtonsCount")
+            ButtonKey1.text = UserDefaults.standard.string(forKey: "ButtonKey1")
+            ButtonKey2.text = UserDefaults.standard.string(forKey: "ButtonKey2")
+            ButtonKey3.text = UserDefaults.standard.string(forKey: "ButtonKey3")
+            ButtonKey4.text = UserDefaults.standard.string(forKey: "ButtonKey4")
+            ButtonKey5.text = UserDefaults.standard.string(forKey: "ButtonKey5")
+            ButtonKey6.text = UserDefaults.standard.string(forKey: "ButtonKey6")
+            ButtonKey7.text = UserDefaults.standard.string(forKey: "ButtonKey7")
+            ButtonKey8.text = UserDefaults.standard.string(forKey: "ButtonKey8")
+            ButtonKey9.text = UserDefaults.standard.string(forKey: "ButtonKey9")
         }
     }
 
@@ -59,14 +113,4 @@ class SettingsLayout: UITableViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    //some stuff
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return false
-    }
-    
-    
-    
 }
